7b7d62f3f0cc28f1169daa147ca16cbd063b307a
Alex Wilson <wilso292@wwu.edu>
2021-12-06 20:28:02 -0800

    A user can now add a new expense and the list of current expenses are dynamically updated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NewComponentExpenses from './components/Expenses/NewComponentExpenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+
+/* Dummy expenses */
+const DUMMY_EXPENSES = [
+  { id: 'e1', title: 'FuboTV', amount: 27.93, date: new Date(2021, 11, 18) },
+  { id: 'e2', title: 'Xbox Game Pass', amount: 15.00, date: new Date(2021, 11, 14) },
+  { id: 'e3', title: 'Xfinity WiFi', amount: 80.93, date: new Date(2021, 11, 1) },
+  { id: 'e4', title: 'Apple News', amount: 5.00, date: new Date(2021, 11, 11) },
+];
+
 const App = () => {
-  const expenses = [
-    { id: 'e1', title: 'FuboTV', amount: 27.93, date: new Date(2021, 11, 18) },
-    { id: 'e2', title: 'Xbox Game Pass', amount: 15.00, date: new Date(2021, 11, 14) },
-    { id: 'e3', title: 'Xfinity WiFi', amount: 80.93, date: new Date(2021, 11, 1) },
-    { id: 'e4', title: 'Apple News', amount: 5.00, date: new Date(2021, 11, 11) },
-  ];
+  const[expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+
 
   const addExpenseHandler = expense => {
     console.log("In App.js SLIME");
-    console.log(expense);
-  }
+
+    /* add the new expense and copy all existing expenses */
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
+  };
 
 
   /* modern and easier way to create component */
