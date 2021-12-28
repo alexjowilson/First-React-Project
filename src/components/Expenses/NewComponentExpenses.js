130018f64611f102149da4a4ef2d84909464e01c
Alex Wilson <wilso292@wwu.edu>
2021-12-28 10:55:58 -0800

    cleaned up code and added conditional content

--- src/components/Expenses/NewComponentExpenses.js
+++ src/components/Expenses/NewComponentExpenses.js
@@ -11,26 +11,38 @@ const NewComponentExpenses = (props) => {
     const selectedNewYear = (selectedYear) =>
     {
         setFilteredYear(selectedYear);
-
         //console.log("The year chosen = " + selectedYear);
-
     }
 
+    /* filter parent array based on year the user selects */
     const filteredExpenses = props.items.filter(expense =>{
         return expense.date.getFullYear().toString() === filteredYear;
     }); 
 
+    /* variable to point to message to user if there is no elements on that year or a filtered array */
+    let expensesContent = <p>No expenses found.</p>;
+
+    /* if length is greater than 0, there is expenses in the year the user selected */
+    if(filteredExpenses.length > 0)
+    {
+        expensesContent = filteredExpenses.map((expense) => (
+            <ExpenseItem 
+                key = {expense.id}
+                title = {expense.title} 
+                amount = {expense.amount}
+                date = {expense.date}
+            />
+        ));
+    }
+
     return (
         <div>
             <Card className="expenses">
-                <ExpenseFilter selected={filteredYear} onSelectedNewYear={selectedNewYear}/> 
-                {filteredExpenses.map((expense) => (
-                    <ExpenseItem 
-                        key = {expense.id}
-                        title = {expense.title} 
-                        amount = {expense.amount}
-                        date = {expense.date}
-                />))}
+                <ExpenseFilter 
+                    selected={filteredYear} 
+                    onSelectedNewYear={selectedNewYear}
+                /> 
+                {expensesContent}
             </Card>
         </div>
     );
